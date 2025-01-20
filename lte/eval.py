bd9bece8db57269a42e0fc8959cc3e0109c652b4
1600421526 <2075279110@qq.com>
2025-01-20 19:29:27 +0800

    main

--- lte/eval.py
+++ lte/eval.py
@@ -1,4 +1,4 @@
-## python lte/eval.py --generation_path save/genration/01M_20D_12H_05m --output_path save/output/01M_20D_11H_31m
+## python lte/eval.py --generation_path save/genration/01M_20D_14H_10m --output_path save/output/01M_20D_11H_31m
 
 import os
 import sys
@@ -18,31 +18,38 @@ logger.add(sys.stdout,
         format="<level>{message}</level>")
 from dataset.utils.benchmark_class import get_benchmark_class
 
-def print_dict_in_table_format(data,task_name_max_len,metric_max_len):
-    column_widths = [task_name_max_len+10, metric_max_len+5, 10]  # 定义每列的宽度
-    header = ["Tasks", "Metric", "Value"]
+def print_dict_in_table_format(data,benchmark_name_max_len,task_name_max_len,metric_max_len):
+    column_widths = [benchmark_name_max_len+2,task_name_max_len+10, metric_max_len+5, 10]  # 定义每列的宽度
+    header = ["BenchMark","Tasks", "Metric", "Value"]
     # 打印表头
     logger.info("|{}|{}|{}|".format(
         header[0].center(column_widths[0], ' '),
         header[1].center(column_widths[1], ' '),
-        header[2].center(column_widths[2], ' ')
+        header[2].center(column_widths[2], ' '),
+        header[2].center(column_widths[3], ' ')
     ))
     logger.info("|{}|{}|{}|".format(
         "-" * column_widths[0],
         "-" * column_widths[1],
-        "-" * column_widths[2]
+        "-" * column_widths[2],
+        "-" * column_widths[3],
+
     ))
-    for task, metrics in data.items():
-        for metric, value in metrics.items():
-            logger.info("|{}|{}|{}|".format(
-                task.center(column_widths[0], ' '),
-                metric.center(column_widths[1], ' '),
-                str(value).center(column_widths[2], ' ')
-            ))
+    for benchmark_name, tasks in data.items():
+        for task, metrics in tasks.items():
+            for metric, value in metrics.items():
+                logger.info("|{}|{}|{}|{}|".format(
+                    benchmark_name.center(column_widths[0], ' '),
+                    task.center(column_widths[1], ' '),
+                    metric.center(column_widths[2], ' '),
+                    str(value).center(column_widths[3], ' ')
+                ))
         logger.info("|{}|{}|{}|".format(
             "-" * column_widths[0],
             "-" * column_widths[1],
-            "-" * column_widths[2]
+            "-" * column_widths[2],
+            "-" * column_widths[3],
+
         ))
 def construct_metrics(metrics_configs):
     for metrics_name,metrics_config in metrics_configs.items():
@@ -53,21 +60,26 @@ def construct_metrics(metrics_configs):
     return metrics_configs
 
 def eval():
+    benchmark_dict = {}
+    benchmark_name_max_len,task_name_max_len,metric_max_len = 0,0,0
     args = handle_cli_args()
     benchmark_list = os.listdir(args.generation_path)
     progress_bar = tqdm(benchmark_list)
     logger.info("*"*40+"  evaluating  "+"*"*40)
     for benchmark_name in progress_bar:
+        benchmark_name_max_len = max(benchmark_name_max_len,len(benchmark_name))
+        benchmark_dict[benchmark_name] = {}
         progress_bar.set_description(f"eval benchmark:{benchmark_name}")
         benchmark = get_benchmark_class(benchmark_name)()
         task_list = os.listdir(args.generation_path+"/"+benchmark_name)
         progress_bar2 = tqdm(task_list)
         for task_name in progress_bar2:
-            progress_bar2.set_description(f"eval task:{task_name}")
+            progress_bar2.set_description(f"eval task:{task_name[:-5]}")
             gathered_metrics = defaultdict(list)
             task_name = task_name[:-5]
+            task_name_max_len = max(task_name_max_len,len(task_name))
             metrics = construct_metrics(benchmark.metric[task_name])
-            save_task_path = os.path.join(args.output_path,task_name)
+            save_task_path = os.path.join(args.output_path,benchmark_name,task_name)
             os.makedirs(save_task_path, exist_ok=True)
             generation_results_path = os.path.join(args.generation_path+"/"+benchmark_name,task_name+".json")
             if not os.path.exists(generation_results_path):
@@ -90,10 +102,13 @@ def eval():
                             gathered_metrics[metric_name].append(score)
             final_metrics = {}
             for metric in gathered_metrics:
+                metric_max_len = max(len(metric),metric_max_len)
                 if metric in ["cite_num"]:
                     final_metrics[metric] = round(np.array(gathered_metrics[metric]).mean(),4)
                 else:
                     final_metrics[metric] = round(100*np.array(gathered_metrics[metric]).mean(),2)
+            
+            benchmark_dict[benchmark_name][task_name] = final_metrics
             logger.info("<<{}>> Final Metric is: {}".format(task_name, final_metrics))
             dump_data = {
                 "task_name": task_name,
@@ -104,19 +119,8 @@ def eval():
                 os.path.join(save_task_path, "final_metrics.json"), "w", encoding="utf-8"
             ) as fout:
                 json.dump(dump_data, fout, indent=4, ensure_ascii=False)
-    save_path = args.output_path
-    metric_list = {}
-    task_name_max_len = 0;metric_max_len=0
-    for task_name in os.listdir(save_path):
-        task_name_max_len = max(task_name_max_len,len(task_name))
-        task_path = os.path.join(save_path ,task_name)
-        if not os.path.exists(os.path.join(task_path,"final_metrics.json")):
-            continue
-        with open(os.path.join(task_path,"final_metrics.json"),"r") as f:
-            result = json.load(f)
-            metric_max_len = max(metric_max_len,len(result["task_name"]))
-            metric_list[result["task_name"]] = result["overall_result"]
-    print_dict_in_table_format(metric_list,task_name_max_len,metric_max_len)
+
+    print_dict_in_table_format(benchmark_dict,benchmark_name_max_len,task_name_max_len,metric_max_len)
     results_table(args.output_path)
 
 if __name__ =="__main__":
