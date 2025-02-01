3369fb0348c4ff553862ad81ba1267022ebfbfb0
1600421526 <2075279110@qq.com>
2025-02-02 00:25:50 +0800

    main

--- models_deploy/rag/__init__.py
+++ models_deploy/rag/__init__.py
@@ -2,7 +2,6 @@ import sys,os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from models_deploy.rag.llamaindex import llamaindex
-from models_deploy.rag.raptor import raptor
 from models_deploy.rag.contriever import contriever
 from models_deploy.rag.BM25 import BM25
 from models_deploy.rag.openai_ import openai 
@@ -10,7 +9,6 @@ RAG_REGISTRY = {
     "llamaindex":llamaindex,
     "BM25":BM25,
     "contriever":contriever,
-    "raptor":raptor,
     "openai":openai
 }
 
