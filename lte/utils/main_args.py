5ce992bf41a289302de7af8d38dd6f28e6583c4d
ZetangForward <zecheng.tang@foxmail.com>
2025-02-18 14:46:45 +0800

    dsa

--- lte/utils/main_args.py
+++ lte/utils/main_args.py
@@ -4,8 +4,8 @@ def handle_cli_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_path", required=True,help = "Selects which model type or provider is evaluated. Must be a string corresponding to the name of the model type/provider being used.")
     parser.add_argument("--rag", default="", choices=["",'openai', 'BM25', 'raptor', 'llamaindex', 'contriever'],help="Selects the model type or provider for information retrieval. You must choose from the following options: 'openai', 'BM25', 'raptor', 'llamaindex', 'contriever'. Each option corresponds to a different approach for retrieving relevant information.")
-    parser.add_argument("--server", type=str, default="transformers",help="Selects which reasoning frame , Must be a string `[transformers,vllm]")
-    parser.add_argument("--acceleration", type=str, default="",help="Selects which acceleration frame , Must be a string:`[duo_attn,Snapkv,streaming_llm,ms-poe,selfextend,lm-inifinite(llama,gpt-j,mpt)，activation_beacon,tova]")
+    parser.add_argument("--server", type=str, default="transformers", choices=['transformers', 'vllm'], help="Selects which reasoning frame , Must be a string `[transformers,vllm]")
+    parser.add_argument("--acceleration", type=str, default="",help="Selects which acceleration frame , Must be a string:`[duo_attn,Snapkv,streaming_llm,ms-poe,selfextend,lm-inifinite(llama,gpt-j,mpt), activation_beacon,tova]")
     parser.add_argument("--device", type=str, default=" ",help=" It is used to set the device on which the model will be placed. The input must be a string, such as `0,1,2,3`.")
     parser.add_argument("--torch_dtype", type=str, default="torch.bfloat16",help="Specifies the PyTorch data type that you want to use. ")
     parser.add_argument("--benchmark_configs", type=str,required=True,help="Specify the benchmark. You should provide a value in the format like '--benchmark_config tasks/General/LongBench'.")
@@ -17,4 +17,5 @@ def handle_cli_args():
     parser.add_argument("--save_tag", type=str, default=None, help="save_dir_name")
     parser.add_argument("--max_lenth",type=int,default="32000",help="The maximum length. It refers to the maximum length of data that a model can load for a single task. If the length exceeds this limit, the data will be split in the middle.")
     args = parser.parse_args()
+    
     return args
